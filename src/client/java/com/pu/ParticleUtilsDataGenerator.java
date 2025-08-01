dae8aacdde8e1e339d657f64ee97e040aa66fa0b
kkkklooooo <mabo27158@gmail.com>
2025-08-01 15:08:25 +0800

    first

---
+++ src/client/java/com/pu/ParticleUtilsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.pu;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ParticleUtilsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
